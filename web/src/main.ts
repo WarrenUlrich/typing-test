8e1b4a4a69a160a3dd787d180763bd2c6c82a77a
Warren Ulrich <warren.ulrich@outlook.com>
2023-12-12 15:41:36 -0800

    updated build script, organized main.ts

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,46 +1,57 @@
 import TypingTest from "./typing_test.js";
 
 let typingTest = await TypingTest.randomWordTest(10);
-
 let wpmValue = document.getElementById('wpmValue') as HTMLElement;
-
 const correctTextElement = document.getElementById('correctText') as HTMLElement;
 const remainingTextElement = document.getElementById('remainingText') as HTMLElement;
 
 remainingTextElement.textContent = typingTest.getOriginalText();
 
+document.addEventListener('keydown', handleTyping);
+
 async function handleTyping(event: KeyboardEvent) {
     if (typingTest.done()) {
-        if (event.key == "Enter") {
-            typingTest = await TypingTest.randomWordTest(10);
-            correctTextElement.innerHTML = '';
-            remainingTextElement.textContent = typingTest.getOriginalText();
-        }
-
+        handleTestCompletion(event);
         return;
     }
 
+    processInput(event);
+}
+
+function processInput(event: KeyboardEvent) {
     typingTest.handleInput(event);
+
     const typedText = typingTest.getTypedText();
     const originalText = typingTest.getOriginalText();
 
+    updateDisplay(typedText, originalText);
+
+    updateWpm(typingTest.calculateWpm());
+}
+
+function updateDisplay(typedText: string, originalText: string) {
     let displayText = '';
+
     for (let i = 0; i < typedText.length; i++) {
-        if (typedText[i] === originalText[i]) {
-            displayText += typedText[i];
-        } else {
-            displayText += `<span class="incorrect">${typedText[i]}</span>`;
-        }
+        displayText += typedText[i] === originalText[i]
+            ? typedText[i]
+            : `<span class="incorrect">${typedText[i]}</span>`;
     }
 
     correctTextElement.innerHTML = displayText; // Use innerHTML since we're adding spans
     remainingTextElement.textContent = originalText.substring(typedText.length);
+}
 
-    let wpm = typingTest.calculateWpm();
-    if (wpm) {
-        console.log("wpm: ", wpm);
-        wpmValue.innerHTML = wpm.toString();
+async function handleTestCompletion(event: KeyboardEvent) {
+    if (event.key == "Enter") {
+        typingTest = await TypingTest.randomWordTest(10);
+        correctTextElement.innerHTML = '';
+        remainingTextElement.textContent = typingTest.getOriginalText();
     }
 }
 
-document.addEventListener('keydown', handleTyping)
+function updateWpm(wpm: number | null) {
+    if (wpm) {
+        wpmValue.innerHTML = wpm.toString();
+    }
+}
